e5535a8c7a53e9d27187960ed7cbf705226667ce
kevintran01 <Kevintran04024@gmail.com>
2018-11-28 16:44:35 -0600

    add finding HTML elements by tag name

--- js.html.dom.js
+++ js.html.dom.js
@@ -1,1 +1,5 @@
 document.getElementById("bodyText").innerHTML = "Greetings Class!";
+
+var x = document.getElementById("main");
+var y = x.getElementsByTagName("p");
+document.getElementById("demo").innerHTML = "The second paragraph (index 1) inside 'main' is: " + y[1].innerHTML;
