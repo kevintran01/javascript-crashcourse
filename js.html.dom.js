855e962d906a2a88111fe138b502749934dec904
kevintran01 <Kevintran04024@gmail.com>
2018-11-28 17:20:11 -0600

    update

--- js.html.dom.js
+++ js.html.dom.js
@@ -3,3 +3,14 @@ document.getElementById("bodyText").innerHTML = "Greetings Class!";
 var x = document.getElementById("main");
 var y = x.getElementsByTagName("p");
 document.getElementById("demo").innerHTML = "The second paragraph (index 1) inside 'main' is: " + y[1].innerHTML;
+
+var element = document.getElementById('id01');
+element.innerText = ("New Heading");
+
+var changePicture = document.getElementById('myImage');
+changePicture.src = "images/sunset1.jpg";
+
+var changeCssStyle = document.getElementById('p2');
+changeCssStyle.style.color = "blue";
+changeCssStyle.style.fontSize = "20px";
+changeCssStyle.style.fontFamily = "Arial";
